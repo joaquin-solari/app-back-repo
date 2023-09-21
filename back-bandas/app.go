7fd3f49819bb940240de6bb25c586725064196ac
Joaquin Solari <joaquinsolari@lenovo-v330-14ikb>
2023-09-21 20:30:52 -0300

    nuevo back

--- back-bandas/app.go
+++ back-bandas/app.go
@@ -1,40 +1,48 @@
 package main
 
 import (
-    "fmt"
-    "math/rand"
-    "net/http"
-    "time"
+	"encoding/json"
+	"fmt"
+	"math/rand"
+	"net/http"
+	"time"
 )
 
 func main() {
-    // Inicializamos la fuente de números aleatorios con una semilla única
-    rand.Seed(time.Now().UnixNano())
+	rand.Seed(time.Now().UnixNano())
 
-    // Lista de nombres de bandas de rock
-    bandasDeRock := []string{
-        "U2",
-        "Arctic Monkeys",
-        "Radiohead",
-        "The Strokes",
-        "RHCP",
-        "Muse",
-        "Oasis",
-        "Airbag",
-        "Catfish and the bottlemen",
-        "The kooks",
-    }
+	bandasDeRock := []string{
+		"U2",
+		"Arctic Monkeys",
+		"Radiohead",
+		"The Strokes",
+		"RHCP",
+		"Muse",
+		"Oasis",
+		"Airbag",
+		"Catfish and the Bottlemen",
+		"The Kooks",
+	}
 
-    // Definimos el handler para el GET request en el root
-    http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-        // Elegimos una banda de rock aleatoria de la lista
-        bandaAleatoria := bandasDeRock[rand.Intn(len(bandasDeRock))]
-        fmt.Fprintf(w, "Banda de rock aleatoria: %s", bandaAleatoria)
-    })
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		// Elegimos una banda de rock aleatoria de la lista
+		bandaAleatoria := bandasDeRock[rand.Intn(len(bandasDeRock))]
 
-    // Iniciamos el servidor en el puerto 8080
-    fmt.Println("Servidor escuchando en el puerto 8080...")
-    if err := http.ListenAndServe(":8080", nil); err != nil {
-        panic(err)
-    }
+		// Crear un mapa para la respuesta en formato JSON
+		response := map[string]string{
+			"banda": bandaAleatoria,
+		}
+
+		// Convertir el mapa en JSON y enviarlo como respuesta
+		w.Header().Set("Content-Type", "application/json")
+		if err := json.NewEncoder(w).Encode(response); err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+	})
+
+	fmt.Println("Servidor escuchando en el puerto 8080...")
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		panic(err)
+	}
 }
---
+++ back-bandas/go.mod
@@ -0,0 +1,3 @@
+module github.com/joaquin-solari/app-back-repo/back-bandas
+
+go 1.12
